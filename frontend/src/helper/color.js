ed5016784882440cae7136bd69b81134d251c7d0
kbpatel <kbpatel3@catamount.wcu.edu>
2023-09-11 21:03:40 -0400

    Fixed color exports. Working now. Search by Node ID

--- frontend/src/helper/color.js
+++ frontend/src/helper/color.js
@@ -5,9 +5,9 @@
  */
 
 // Colors to use throughout the app 
-var illicit = "rgb(184, 67, 4)"
-var licit =   "rgb(44, 165, 104)"
-var unknown = "rgb(128, 128, 128)"
+const ILLICIT = "rgb(184, 67, 4)"
+const LICIT =   "rgb(44, 165, 104)"
+const UNKNOWN = "rgb(128, 128, 128)"
 
 /**
  * Matchs the group value of a node and returns the corect coloring
@@ -15,9 +15,18 @@ var unknown = "rgb(128, 128, 128)"
  * @returns The corresponding color
  */
 const getColor = (group) => {
-    if (group == "1") return illicit;
-    if (group == "2") return licit;
-    if (group == "3") return unknown;
+    if (group === "1") {
+        console.log(ILLICIT);
+        return ILLICIT;
+    }
+    if (group === "2") {
+        console.log(LICIT);
+        return LICIT;
+    }
+    if (group === "3") {
+        console.log(UNKNOWN);
+        return UNKNOWN;
+    }
 
     // Default color
     return "rgb(255, 0, 255)";
@@ -27,36 +36,36 @@ const getColor = (group) => {
  * Sets the color for the illicit group
  * @param {string} color The RGB color to set
  */
-const setIllicit = (color) => {
-    illicit = color;
-}
+// const setIllicit = (color) => {
+//     illicit = color;
+// }
 
 /**
  * Sets the color for the licit group
  * @param {string} color The RGB color to set
  */
-const setLicit = (color) => {
-    licit = color;
-}
+// const setLicit = (color) => {
+//     licit = color;
+// }
 
 /**
  * Sets the color for the unknown group
  * @param {string} color The RGB color to set
  */
-const setUnknown = (color) => {
-    unknown = color;
-}
+// const setUnknown = (color) => {
+//     unknown = color;
+// }
 
 /**
  * Sets the color for a group
  * @param {string} group The group to set the color for
  * @param {string} color The RGB color to set
  */
-const setColor = (group, color) => {
-    if (group == "1") illicit = color;
-    if (group == "2") licit = color;
-    if (group == "3") unknown = color;
-}
+// const setColor = (group, color) => {
+//     if (group === "1") illicit = color;
+//     if (group === "2") licit = color;
+//     if (group === "3") unknown = color;
+// }
 
 
 
@@ -64,4 +73,5 @@ const setColor = (group, color) => {
 // setColor("2", "rgb(44, 165, 104)")
 // setColor("3", "rgb(128, 128, 128)")
 
-export { getColor, setColor };
+//export { getColor, setColor };
+export default getColor;
