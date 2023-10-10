bbb07c98ea90f8718674571e922679f803ff7c16
kbpatel <kbpatel3@catamount.wcu.edu>
2023-10-10 10:20:52 -0400

    Documentation and JSX formatting.

--- frontend/src/components/SubGraph.js
+++ frontend/src/components/SubGraph.js
@@ -1,10 +1,28 @@
+/**
+ * SubGraph component, used to render the subgraph of a specific node
+ * @author Kaushal Patel
+ * @author Noah Hasset
+ */
+
 import { useReadCypher } from "use-neo4j";
 import { useEffect, useState } from "react";
 import SubGraphVisual from "./SubGraphVisual";
 
+/**
+ * SubGraph component, used to render the subgraph of a specific node
+ * @param clickedNode - the node that was clicked on
+ * @returns {JSX.Element} - the subgraph of the clicked node
+ * @constructor - the subgraph component
+ */
 export default function SubGraph({ clickedNode }) {
+  // Constants used for talking to the database
   const key = "{nodes: nodes, links: links}";
 
+  /**
+   * Takes a node and builds a query to get the data from the database
+   * @param v - the node that was clicked on
+   * @returns {string} - the query to be sent to the database
+   */
   const getQuery = (v) => {
     const query = `MATCH path = (n {id: "${v}"})-[*]-(m)
      WHERE id(n) <> id(m)
@@ -17,10 +35,13 @@ export default function SubGraph({ clickedNode }) {
     return query;
   };
 
+  // Get the query
   let query = getQuery(clickedNode);
 
+  // Get the functions and variables we need from the use-neo4j package
   const { loading, error, records, run } = useReadCypher(query);
 
+  // Requery the database whenever the state of the clicked node changes
   useEffect(() => {
     query = getQuery(clickedNode);
     run({ query });
@@ -28,24 +49,33 @@ export default function SubGraph({ clickedNode }) {
     if (error) console.log("Error");
   }, [clickedNode]);
 
+  // Init our data
   let data = undefined;
 
+  // Check to see if the data has been assigned by the database yet
   if (records === undefined) {
+    // Debug to see if the data has been assigned yet
     console.log("Records is undefined");
   } else {
+    // If the data has finished retrieving from the database, assign it to the rawResult variable
     const rawResult = records[0].get(key);
+
     // Remove duplicate nodes by converting them to a Map
     const uniqueNodesMap = new Map(
       rawResult.nodes.map((node) => [node.id, node]),
     );
+
     // Convert the unique nodes back to an array
     const uniqueNodes = Array.from(uniqueNodesMap.values());
 
-    // Now, you have uniqueNodes without duplicates
+    // Assign the data to the data variable
     data = { ...rawResult, nodes: uniqueNodes };
+
+    // Debug to see the data
     console.log(data);
   }
 
+  // Return the SubGraphVisual component if the data has been loaded
   return (
     <div>
       {data ? (
@@ -68,7 +98,8 @@ export default function SubGraph({ clickedNode }) {
         //     ))}
         //   </ul>
         // </div>
-          <SubGraphVisual data={data} highlight={clickedNode} />
+        // Render the subgraph visual with the data and the clicked node
+        <SubGraphVisual data={data} highlight={clickedNode.id} />
       ) : (
         <p>Loading data...</p>
       )}
--- frontend/src/components/SubGraphVisual.js
+++ frontend/src/components/SubGraphVisual.js
@@ -1,13 +1,27 @@
+/**
+ * SubGraphVisual component, used to generate the subgraph visual using D3
+ * @author Kaushal Patel
+ * @author Noah Hasset
+ */
+
 import useD3 from "../hooks/useD3";
-import * as d3 from 'd3';
+import * as d3 from "d3";
 import getColor from "../helper/color";
 import "./graph.css";
 
-const SubGraphVisual = ({data, highlight}) => {
-
+/**
+ * SubGraphVisual component, used to generate the subgraph visual using D3
+ * @param data - data to be used to generate the graph
+ * @param highlight - node to be highlighted
+ * @returns {JSX.Element} - SubGraphVisual component
+ * @constructor - SubGraphVisual
+ */
+const SubGraphVisual = ({ data, highlight }) => {
+  // D3 reference
   const ref = useD3(
     (svg) => {
-        if (!data) return;
+      // If there is no data, return
+      if (!data) return;
 
       // Get the parent elements dimentions
       const dimensions = d3.select(".subgraph").node().getBoundingClientRect();
@@ -27,112 +41,122 @@ const SubGraphVisual = ({data, highlight}) => {
 
       // Define how we would like our simulation to act
       const simulation = d3
-          .forceSimulation(data.nodes)
-          .force("link", d3.forceLink().id(d => d.id))
-          .force("charge", d3.forceManyBody().strength(-1))
-          .force("center", d3.forceCenter(width / 2, height / 2));
+        .forceSimulation(data.nodes)
+        .force(
+          "link",
+          d3.forceLink().id((d) => d.id),
+        )
+        .force("charge", d3.forceManyBody().strength(-1))
+        .force("center", d3.forceCenter(width / 2, height / 2));
 
-        // Functions to define what happens when a user clicks on an app
+      // Functions to define what happens when a user clicks on an app
 
-        function dragstarted(event) {
-            if (!event.active) simulation.alphaTarget(0.3).restart();
-            event.subject.fx = event.subject.x;
-            event.subject.fy = event.subject.y;
-        }
+      /**
+       * Drag started function, defines what happens when a user starts to drag a node
+       * @param event - event that triggered the function
+       */
+      function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      }
 
-        function dragged(event) {
-          event.subject.fx = event.x;
-          event.subject.fy = event.y;
-        }
+      /**
+       * Dragged function, defines what happens when a user drags a node
+       * @param event - event that triggered the function
+       */
+      function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }
 
-        function dragended(event) {
-          if (!event.active) simulation.alphaTarget(0);
-          event.subject.fx = null;
-          event.subject.fy = null;
-        }
+      /**
+       * Drag ended function, defines what happens when a user stops dragging a node
+       * @param event - event that triggered the function
+       */
+      function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }
 
-        // Defines our links (edges) on the screen
-        const link = svg
-          .append("g")
-          .attr("class", "links")
-          .selectAll("line")
-          .data(data.links)
-          .enter()
-          .append("line")
-          .attr("stroke-width", function(d) {
-            return Math.sqrt(d.value);
-          });
+      // Defines our links (edges) on the screen
+      const link = svg
+        .append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(data.links)
+        .enter()
+        .append("line")
+        .attr("stroke-width", function (d) {
+          return Math.sqrt(d.value);
+        });
 
-        // Defines our nodes on the screen
-        const node = svg
-          .append("g")
-          .attr("class", "nodes")
-          .selectAll("circle")
-          .data(data.nodes)
-          .enter()
-          .append("circle")
-          .attr("id", (d) => `node${d.id}`)
-          .attr("r", 7)
-          .attr("fill", (d) => getColor(d.group))
-          .call(
-            d3
-              .drag()
-              .on("start", dragstarted)
-              .on("drag", dragged)
-              .on("end", dragended)
-            );
+      // Defines our nodes on the screen
+      const node = svg
+        .append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(data.nodes)
+        .enter()
+        .append("circle")
+        .attr("id", (d) => `node${d.id}`)
+        .attr("r", 7)
+        .attr("fill", (d) => getColor(d.group))
+        .call(
+          d3
+            .drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended),
+        );
 
-        // Adds titles to nodes
-        node.append("title").text((d) => { return d.id; });
+      // Adds titles to nodes
+      node.append("title").text((d) => {
+        return d.id;
+      });
 
       // Defines the action for how nodes act over time
       const limitPosition = (value, direction) => {
         if (value < 0) return 0;
-        else if(direction === "x") return value > width ? width : value;
-        else if(direction === "y") return value > height ? height : value;
-      }
+        else if (direction === "x") return value > width ? width : value;
+        else if (direction === "y") return value > height ? height : value;
+      };
 
       /**
        * Ticked function, defines what happens to the nodes as the simulation "ticks"
        */
       const ticked = () => {
         link
-          .attr("x1", (d) => limitPosition(d.source.x, "x") )
-          .attr("y1", (d) => limitPosition(d.source.y, "y") )
-          .attr("x2", (d) => limitPosition(d.target.x, "x") )
-          .attr("y2", (d) => limitPosition(d.target.y, "y") );
+          .attr("x1", (d) => limitPosition(d.source.x, "x"))
+          .attr("y1", (d) => limitPosition(d.source.y, "y"))
+          .attr("x2", (d) => limitPosition(d.target.x, "x"))
+          .attr("y2", (d) => limitPosition(d.target.y, "y"));
 
         node
-          .attr("cx", (d) => limitPosition(d.x, "x") )
-          .attr("cy", (d) => limitPosition(d.y, "y") );
-      }
+          .attr("cx", (d) => limitPosition(d.x, "x"))
+          .attr("cy", (d) => limitPosition(d.y, "y"));
+      };
 
       // Add the ticked method, nodes and links to our simulation
-      simulation
-        .nodes(data.nodes)
-        .on("tick", ticked);
+      simulation.nodes(data.nodes).on("tick", ticked);
 
-      simulation
-        .force("link")
-        .links(data.links);
+      simulation.force("link").links(data.links);
 
       // Calls the clicked node funciton with the specified highlighted node
       clickNode(highlight);
     },
     // the data to be watched for changes
-    [data]
+    [data],
   );
 
   // SVG Containing the graph
   return (
     <div className="graphContainer h-full w-full">
-      <svg
-        ref={ref}
-        className="inline-block absolute">
-      </svg>
+      <svg ref={ref} className="inline-block absolute"></svg>
     </div>
   );
-}
+};
 
 export default SubGraphVisual;
 
@@ -142,5 +166,7 @@ export default SubGraphVisual;
  */
 function clickNode(id) {
   // Resets all the nodes in the graph and set them to the correct size and color
-  d3.selectAll('circle').attr('fill', (d) => getColor(d.group)).attr('r', 7);
+  d3.selectAll("circle")
+    .attr("fill", (d) => getColor(d.group))
+    .attr("r", 7);
 }
