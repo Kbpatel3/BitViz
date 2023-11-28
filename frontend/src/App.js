092e16d1736a65513093bebb2d637311b3c225a7
kbpatel <kbpatel3@catamount.wcu.edu>
2023-11-28 11:38:33 -0500

    Fixed pie chart dimension scaling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -169,7 +169,7 @@ function App() {
                                 <BarWrapper timestep={timestep}/>
                             </div>
                         )}
-                        {graph === "Pie" && <Pie data={data.nodes} h={window.outerHeight/3 < 270 ? 270 : window.outerHeight/3}/>}
+                        {graph === "Pie" && <Pie data={data.nodes}/>}
                     </div>
                 ) : (
                     <div className="h-80 row-span-3 col-start-3 row-start-2 bg-slate-200 hover:bg-slate-300">
