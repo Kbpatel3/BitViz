32fb06e652524ada2d7ca7bcff96eeb868078dfe
kbpatel <kbpatel3@catamount.wcu.edu>
2024-04-23 11:21:26 -0400

    Merged fix

--- frontend/src/components/FilterGraphButton.js
+++ frontend/src/components/FilterGraphButton.js
@@ -2,7 +2,7 @@
  * Defines a FilterGraphButton Component
  * @author Noah Hassett
  * @author Kaushal Patel
- *  
+ *
  */
 import filter from "../media/filter_icon.png";
 import { useState } from "react";
@@ -44,9 +44,9 @@ export default function FilterGraphButton({ handleGraphSwitch, setBarMax }) {
       {showFilterPanel && (
         <div className="flex justify-between items-center space-x-1 ml-2">
           <select
-              className="h-7 inline-block rounded-full border-2 border-primary px-3 pb-[3px] pt-1 
-              text-xs font-medium uppercase leading-normal text-primary focus:outline-none 
-              focus:ring-0 active:border-primary-700 active:text-primary-700 
+              className="h-7 inline-block rounded-full border-2 border-primary px-3 pb-[3px] pt-1
+              text-xs font-medium uppercase leading-normal text-primary focus:outline-none
+              focus:ring-0 active:border-primary-700 active:text-primary-700
               motion-reduce:transition-none dark:text-primary-500"
               onChange={handleSelectChange}
               value={selectedFilter}
@@ -61,11 +61,11 @@ export default function FilterGraphButton({ handleGraphSwitch, setBarMax }) {
           <button
             type="button"
             onClick={applyFilter}
-            className="h-7 border-slate-600 inline-block rounded-full border-2 border-primary px-3 
-            pb-[6px] pt-1 text-xs font-medium uppercase leading-normal text-primary transition 
-            duration-150 ease-in-out hover:border-primary-accent-300 hover:bg-primary-50/50 
-            hover:text-primary-accent-300 focus:border-primary-600 focus:bg-primary-50/50 
-            focus:text-primary-300 focus:outline-none focus:ring-0 active:border-primary-700 
+            className="h-7 border-slate-600 inline-block rounded-full border-2 border-primary px-3
+            pb-[6px] pt-1 text-xs font-medium uppercase leading-normal text-primary transition
+            duration-150 ease-in-out hover:border-primary-accent-300 hover:bg-primary-50/50
+            hover:text-primary-accent-300 focus:border-primary-600 focus:bg-primary-50/50
+            focus:text-primary-300 focus:outline-none focus:ring-0 active:border-primary-700
             active:text-primary-700 motion-reduce:transition-none dark:text-primary-500">
             Apply
           </button>
